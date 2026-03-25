ab4300448800d29617afb503f41ba28febea1e58
shenminee <minamuksl51@gmail.com>
2026-03-25 08:41:30 +0700

    Delete client.js

--- client.js
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-export function printBanner() {
-    console.log(chalk.blueBright("Modified Baileys By Bian Official 🌟"));
-    console.log(chalk.cyanBright("Telegram  : ") + chalk.whiteBright("https://t.me/phyrinee"));
-    console.log(chalk.greenBright("Whatsapp  : ") + chalk.whiteBright("https://wa.me/6285189416130"));
-    console.log(chalk.magentaBright("Group     : ") + chalk.whiteBright("https://urli.info/1rs3d"));
-}
